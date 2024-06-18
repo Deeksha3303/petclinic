69d6ab21d41111ba5065888a51967ae3f03ca2ae
Chandana N <133877849+chan-dana@users.noreply.github.com>
2024-06-18 23:01:00 +0530

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,7 @@ export const products = [
         name: "Veterinary Services",
         price: 19.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image:"images/banner/bag_01.jpeg",
+        image:"images/banner/products/bag_01.jpeg",
         
        
     },
@@ -14,34 +14,34 @@ export const products = [
         name: "Grooming",
         price: 35,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "images/banner/bag_02.png"
+        image: "images/banner/products/bag_02.png"
     },
     {
         id: 3,
         name: "Pet-Sitting",
         price: 10,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "images/banner/bag_03.png"
+        image: "images/banner/products/bag_03.png"
     },
     {
         id: 4,
         name: "Pet Training",
         price: 29.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "images/banner/bag_04.jpg"
+        image: "images/banner/products/bag_04.jpg"
     },
     {
         id: 5,
         name: "Nutrition and Diet Services",
         price: 27.99,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "images/banner/bag_05.jpeg"
+        image: "images/banner/products/bag_05.jpeg"
     },
     {
         id: 6,
         name: "Emergency Care",
         price: 50,
         description: "Torem ipsum dolor sit amet, consectetur adipisicing elitsed do eiusmo tempor incididunt ut labore et dolore magna",
-        image: "images/banner/bag_06.png"
+        image: "images/banner/products/bag_06.png"
     }
 ]
