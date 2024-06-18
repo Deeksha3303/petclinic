979c7437624b702c0bc4c125cfe5ba32c4f3dd61
Chandana N <133877849+chan-dana@users.noreply.github.com>
2024-06-18 22:37:03 +0530

    Update SingleProductDesktop.js

--- src/components/products/SingleProductDesktop.js
+++ src/components/products/SingleProductDesktop.js
@@ -1,7 +1,3 @@
-
-import FavoriteBorderOutlined from "@mui/icons-material/FavoriteBorderOutlined";
-import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import FitScreenOutlinedIcon from '@mui/icons-material/FitScreenOutlined';
 import { Stack } from "@mui/material"
 import {    ProductActionsWrapper, Product, ProductActionButton, ProductAddToCart, ProductFavButton, ProductImage, Productmetawrapper } from "../../styles/product"
 import ProductMeta from "./ProductMeta";
@@ -25,21 +21,8 @@ export default function SingleProductDesktop ({product, matches}){
   
      <Product onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
             <ProductImage src={product.image}></ProductImage>
-            <ProductFavButton isfav={1}>
-                    <FavoriteBorderOutlined ></FavoriteBorderOutlined> 
-                </ProductFavButton>
-                {showOptions &&( <ProductAddToCart variant="contained"  show={showOptions}>  Add to cart</ProductAddToCart>)}
-            <ProductActionsWrapper show={showOptions} >
-                <Stack direction="column">
-               
-                <ProductActionButton>
-                <ShareOutlinedIcon></ShareOutlinedIcon>
-                </ProductActionButton>
-                <ProductActionButton>
-                    <FitScreenOutlinedIcon></FitScreenOutlinedIcon>
-                </ProductActionButton>
-                </Stack>
-            </ProductActionsWrapper>
+                {showOptions &&( <ProductAddToCart variant="contained"  show={showOptions}>  BOOK </ProductAddToCart>)}
+      
         </Product>
         <ProductMeta product={product} matches={matches}></ProductMeta>
         
