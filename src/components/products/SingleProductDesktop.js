232417b13d8412e94ea03055c92d399cf7c6a764
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-06-19 10:41:41 +0530

    Update SingleProductDesktop.js

--- src/components/products/SingleProductDesktop.js
+++ src/components/products/SingleProductDesktop.js
@@ -1,5 +1,5 @@
 import { Stack } from "@mui/material"
-import {    ProductActionsWrapper, Product, ProductActionButton, ProductAddToCart, ProductFavButton, ProductImage, Productmetawrapper } from "../../styles/product"
+import {    ProductActionsWrapper, Product, ProductActionButton, ProductAddToCart, ProductFavButton, ProductImage} from "../../styles/product"
 import ProductMeta from "./ProductMeta";
 import { useState } from "react";
 
