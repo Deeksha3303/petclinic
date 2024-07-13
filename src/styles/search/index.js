48c0e4b02b03a145c8a660c22e4232cdb02d9801
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-07-13 21:51:36 +0530

    Update index.js

--- src/styles/search/index.js
+++ src/styles/search/index.js
@@ -1,5 +1,5 @@
 import { styled } from "@mui/material/styles"
-import{flex,Box, TextField} from "@mui/material";
+import{Box, TextField} from "@mui/material";
 import { Colors } from "../theme";
 
  
