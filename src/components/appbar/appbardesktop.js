d8c7903c9a30851e8bd29deeac05a4bb6bb7118b
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-07-13 21:19:44 +0530

    Update appbardesktop.js

--- src/components/appbar/appbardesktop.js
+++ src/components/appbar/appbardesktop.js
@@ -1,13 +1,10 @@
-import {  ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import { AppbarContainer, Appheader, MyList } from "../../styles/appbar";
-import SearchIcon from '@mui/icons-material/Search';
+import { AppbarContainer, Appheader } from "../../styles/appbar";
 import Actions from "./actions";
 import { Colors } from "../../styles/theme";
 import { ContexUi } from "../../context/contexUI";
 
 export default function AppBarDesktop({matches}){
     
-    const {setSearchBox}= ContexUi();
     return(
         <AppbarContainer>
             <Appheader color={"#d1adcc"}>Pawprints</Appheader>
