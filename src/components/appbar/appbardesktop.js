1cb35b2b4b2c88b4fefd538cfdca70fb5caf4abf
Deeksha3303 <106835468+Deeksha3303@users.noreply.github.com>
2024-07-13 21:40:19 +0530

    Update appbardesktop.js

--- src/components/appbar/appbardesktop.js
+++ src/components/appbar/appbardesktop.js
@@ -1,13 +1,19 @@
+import PropTypes from 'prop-types';
 import { AppbarContainer, Appheader } from "../../styles/appbar";
 import Actions from "./actions";
-import { Colors } from "../../styles/theme";
 
-export default function AppBarDesktop({matches}){
-    
-    return(
+function AppBarDesktop({ matches }) {
+    return (
         <AppbarContainer>
             <Appheader color={"#d1adcc"}>Pawprints</Appheader>
-            <Actions matches={matches}></Actions>
+            <Actions matches={matches} />
         </AppbarContainer>
-    )
- }
+    );
+}
+
+// Define propTypes for the component
+AppBarDesktop.propTypes = {
+    matches: PropTypes.bool.isRequired, // Ensure matches is a required boolean prop
+};
+
+export default AppBarDesktop;
