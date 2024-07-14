56d198d87a8c4614fee661d0870862467b7b7707
Chandana N <133877849+chan-dana@users.noreply.github.com>
2024-07-14 19:38:20 +0530

    Update actions.js

--- src/components/appbar/actions.js
+++ src/components/appbar/actions.js
@@ -1,34 +1,35 @@
+import PropTypes from 'prop-types';
 import { Divider, ListItemButton, ListItemIcon } from "@mui/material";
 import { ActionsAppDesktop, ActionsAppMobile, MyList } from "../../styles/appbar";
 import { Colors } from "../../styles/theme";
 
- 
- export default function Actions({matches}){
-    const Components = matches ? ActionsAppMobile:ActionsAppDesktop
-    return(
+function Actions({ matches }) {
+    const Components = matches ? ActionsAppMobile : ActionsAppDesktop;
+    return (
         <Components>
-            <MyList type= "row">
-            <ListItemButton sx={{justifyContent:"center"}}>
-                <ListItemIcon sx={{display:"flex",justifyContent:"center",color:matches&& Colors.secondary}}>
-                 
-                    <Divider orientation="vertical" flexItem></Divider>
-                </ListItemIcon>
-            </ListItemButton>
-           
-            <ListItemButton sx={{justifyContent:"center"}}>
-                <ListItemIcon sx={{display:"flex",justifyContent:"center",color:matches&& Colors.secondary}}>
-                   
-                    <Divider orientation="vertical" flexItem></Divider>
-                </ListItemIcon>
-            </ListItemButton>
-            <ListItemButton sx={{justifyContent:"center"}}>
-                <ListItemIcon sx={{display:"flex",justifyContent:"center",color:matches&& Colors.secondary}}>
-                   
-                    <Divider orientation="vertical" flexItem></Divider>
-                </ListItemIcon>
-            </ListItemButton>
-        </MyList>
+            <MyList type="row">
+                <ListItemButton sx={{ justifyContent: "center" }}>
+                    <ListItemIcon sx={{ display: "flex", justifyContent: "center", color: matches && Colors.secondary }}>
+                        <Divider orientation="vertical" flexItem />
+                    </ListItemIcon>
+                </ListItemButton>
+                <ListItemButton sx={{ justifyContent: "center" }}>
+                    <ListItemIcon sx={{ display: "flex", justifyContent: "center", color: matches && Colors.secondary }}>
+                        <Divider orientation="vertical" flexItem />
+                    </ListItemIcon>
+                </ListItemButton>
+                <ListItemButton sx={{ justifyContent: "center" }}>
+                    <ListItemIcon sx={{ display: "flex", justifyContent: "center", color: matches && Colors.secondary }}>
+                        <Divider orientation="vertical" flexItem />
+                    </ListItemIcon>
+                </ListItemButton>
+            </MyList>
         </Components>
-        
-    )
- }
+    );
+}
+
+Actions.propTypes = {
+    matches: PropTypes.bool.isRequired,  // Add 'matches' prop with PropTypes.bool
+};
+
+export default Actions;
