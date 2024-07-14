f5aa8b0f762f60348f179b8a5ad5c1d0e3c8d610
Anwitha Sudheer <163495068+Anwi1242@users.noreply.github.com>
2024-07-14 21:05:08 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // For additional Jest matchers
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  test('renders App component', () => {
+    const { getByText } = render(<App />);
+
+    // Check for specific text or components rendered by App
+    expect(getByText('Our Services')).toBeInTheDocument(); // Line 16
+    expect(getByText('E-bag shop')).toBeInTheDocument(); // Line 17
+    expect(getByText('Products')).toBeInTheDocument(); // Line 22
+  });
 });
