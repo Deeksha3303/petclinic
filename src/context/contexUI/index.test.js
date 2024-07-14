7730a3404c68c137e9c402eb421f6e51b92ca863
Anwitha Sudheer <163495068+Anwi1242@users.noreply.github.com>
2024-07-14 23:45:23 +0530

    Update index.test.js

--- src/context/contexUI/index.test.js
+++ src/context/contexUI/index.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { UIprovider, UIcontext, ContexUi } from './UIprovider';
+import { UIprovider, ContexUi } from './UIprovider';
 
 describe('UIprovider', () => {
   it('renders UIprovider component with children', () => {
