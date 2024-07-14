49a9f6514bd2b5a21a0b72ea3de09c6c58197af7
Anwitha Sudheer <163495068+Anwi1242@users.noreply.github.com>
2024-07-14 19:52:35 +0530

    Update SingleProduct.js

--- src/components/products/SingleProduct.js
+++ src/components/products/SingleProduct.js
@@ -1,48 +1,42 @@
+import PropTypes from 'prop-types';
 import FavoriteBorderOutlined from "@mui/icons-material/FavoriteBorderOutlined";
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import FitScreenOutlinedIcon from '@mui/icons-material/FitScreenOutlined';
-import { Stack } from "@mui/material"
-import {    ProductActionsWrapper, Product, ProductActionButton, ProductAddToCart, ProductFavButton, ProductImage, Productmetawrapper } from "../../styles/product"
-import ProductMeta from "./ProductMeta"
+import { Stack } from "@mui/material";
+import { ProductActionsWrapper, Product, ProductActionButton, ProductAddToCart, ProductFavButton, ProductImage, ProductMetaWrapper } from "../../styles/product";
+import ProductMeta from "./ProductMeta";
 
-
-
-
-export default function SingleProduct ({product, matches}){
-    
-
-   
-
+export default function SingleProduct({ product, matches }) {
     return (
-    <>
-      
-    <Product >
-            <ProductImage src={product.image}></ProductImage>
-           
-            
-            <ProductMeta product={product} matches={matches}></ProductMeta>
-           
-            <ProductActionsWrapper >
-                <Stack direction="row">
-                <ProductFavButton isfav={1}>
-                    <FavoriteBorderOutlined ></FavoriteBorderOutlined> 
-                </ProductFavButton>
-                <ProductActionButton>
-                <ShareOutlinedIcon></ShareOutlinedIcon>
-                </ProductActionButton>
-                <ProductActionButton>
-                    <FitScreenOutlinedIcon></FitScreenOutlinedIcon>
-                </ProductActionButton>
-                </Stack>
-            </ProductActionsWrapper>
-            <ProductAddToCart > BOOK </ProductAddToCart>
-            
-        </Product>
-        
+        <>
+            <Product>
+                <ProductImage src={product.image}></ProductImage>
+                <ProductMeta product={product} matches={matches}></ProductMeta>
+                <ProductActionsWrapper>
+                    <Stack direction="row">
+                        <ProductFavButton isfav={1}>
+                            <FavoriteBorderOutlined />
+                        </ProductFavButton>
+                        <ProductActionButton>
+                            <ShareOutlinedIcon />
+                        </ProductActionButton>
+                        <ProductActionButton>
+                            <FitScreenOutlinedIcon />
+                        </ProductActionButton>
+                    </Stack>
+                </ProductActionsWrapper>
+                <ProductAddToCart>BOOK</ProductAddToCart>
+            </Product>
         </>
-        
-        
-    )
-
-
+    );
 }
+
+// Define prop types
+SingleProduct.propTypes = {
+    product: PropTypes.shape({
+        image: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+    }).isRequired,
+    matches: PropTypes.bool.isRequired,
+};
