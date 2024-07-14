961a899f145fd1a1f8b8a6f133dd2a6db348be42
Anwitha Sudheer <163495068+Anwi1242@users.noreply.github.com>
2024-07-14 19:45:42 +0530

    Update appbarmobile.js

--- src/components/appbar/appbarmobile.js
+++ src/components/appbar/appbarmobile.js
@@ -1,23 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { IconButton } from "@mui/material";
 import { AppbarContainer, Appheader } from "../../styles/appbar";
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import Actions from "./actions";
+import { useContext } from 'react';
 import { ContexUi } from "../../context/contexUI";
 
- export default function AppBarMobile({matches}){
-     const {setdrawer ,setSearchBox} = ContexUi();
-    return(
+export default function AppBarMobile({ matches }) {
+    const { setDrawer, setSearchBox } = useContext(ContexUi);
+    
+    return (
         <AppbarContainer>
-            <IconButton onClick={()=>setdrawer(true)}>
-                <MenuOutlinedIcon></MenuOutlinedIcon>
+            <IconButton onClick={() => setDrawer(true)}>
+                <MenuOutlinedIcon />
             </IconButton>
             <Appheader variant="h4" textAlign={"center"} color={"#d1adcc"}>My Bags</Appheader>
-            <IconButton onClick={()=>setSearchBox(true)}>
-            <SearchOutlinedIcon ></SearchOutlinedIcon>
+            <IconButton onClick={() => setSearchBox(true)}>
+                <SearchOutlinedIcon />
             </IconButton>
-            <Actions matches={matches}></Actions>
-            
+            <Actions matches={matches} />
         </AppbarContainer>
-    )
- }
+    );
+}
+
+// Define prop types
+AppBarMobile.propTypes = {
+    matches: PropTypes.bool.isRequired,
+};
