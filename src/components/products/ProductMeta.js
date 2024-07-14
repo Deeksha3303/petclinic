62e7df4c8f1f0d93fbd6bb4f84c2c2d17a42e0ad
Anwitha Sudheer <163495068+Anwi1242@users.noreply.github.com>
2024-07-14 19:37:53 +0530

    Update ProductMeta.js

--- src/components/products/ProductMeta.js
+++ src/components/products/ProductMeta.js
@@ -1,15 +1,23 @@
+import PropTypes from 'prop-types';
 import { Typography } from "@mui/material";
 import { ProductMetaWrapper } from "../../styles/product";
 
-export default function ProductMeta({matches, product}){
-
-    return(
+export default function ProductMeta({ matches, product }) {
+    return (
         <ProductMetaWrapper>
-            <Typography variant={ matches?"h6":"h5"}>{product.name}</Typography>
-        <Typography variant = { matches? "caption":"body1"}>
-            ${product.price}
-        </Typography>
+            <Typography variant={matches ? "h6" : "h5"}>{product.name}</Typography>
+            <Typography variant={matches ? "caption" : "body1"}>
+                ${product.price}
+            </Typography>
         </ProductMetaWrapper>
-        
-    )
+    );
 }
+
+// Define prop types
+ProductMeta.propTypes = {
+    matches: PropTypes.bool.isRequired,
+    product: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+    }).isRequired,
+};
