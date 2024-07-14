6b8c6051d508eeb0b437f680ce8ec5ae9dd56fd8
Chandana N <133877849+chan-dana@users.noreply.github.com>
2024-07-14 19:51:07 +0530

    Update SingleProductDesktop.js

--- src/components/products/SingleProductDesktop.js
+++ src/components/products/SingleProductDesktop.js
@@ -1,34 +1,40 @@
-import { Product,  ProductAddToCart, ProductImage} from "../../styles/product"
+import PropTypes from 'prop-types';
+import { Product, ProductAddToCart, ProductImage } from "../../styles/product";
 import ProductMeta from "./ProductMeta";
 import { useState } from "react";
 
+export default function SingleProductDesktop({ product, matches }) {
+    const [showOptions, setShowOptions] = useState(false);
 
+    const handleMouseEnter = () => {
+        setShowOptions(true);
+    };
 
-export default function SingleProductDesktop ({product, matches}){
-    const [showOptions, setShowOptions]= useState(false);
-
-    const handleMouseEnter=()=>{
-        setShowOptions(true)
-    }
-    const handleMouseLeave=()=>{
-        setShowOptions(false)
-    }
-   
+    const handleMouseLeave = () => {
+        setShowOptions(false);
+    };
 
     return (
-    <>
-  
-     <Product onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
-            <ProductImage src={product.image}></ProductImage>
-                {showOptions &&( <ProductAddToCart variant="contained"  show={showOptions}>  BOOK </ProductAddToCart>)}
-      
-        </Product>
-        <ProductMeta product={product} matches={matches}></ProductMeta>
-        
-    </>
-       
-        
-    )
-
-
+        <>
+            <Product onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <ProductImage src={product.image} />
+                {showOptions && (
+                    <ProductAddToCart variant="contained" show={showOptions}>
+                        BOOK
+                    </ProductAddToCart>
+                )}
+            </Product>
+            <ProductMeta product={product} matches={matches} />
+        </>
+    );
 }
+
+// Define propTypes to validate props
+SingleProductDesktop.propTypes = {
+    matches: PropTypes.bool.isRequired,
+    product: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,  // Ensure 'image' is a required string
+    }).isRequired,
+};
